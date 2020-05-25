cc3e46a7d1864b8c618820bc9068887b3560f2c8
Fabricio <fabricxx@gmail.com>
2020-05-25 04:17:20 -0300

    .

---
+++ src/application/CalculadoraController.java
@@ -0,0 +1,55 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CalculadoraController {
+	@FXML
+	private TextField TelaCalculadora;
+	@FXML
+	private Button bt8;
+	@FXML
+	private Button btparentesesD;
+	@FXML
+	private Button btApagar;
+	@FXML
+	private Button bt4;
+	@FXML
+	private Button btPorsentagem;
+	@FXML
+	private Button btDivisao;
+	@FXML
+	private Button bt9;
+	@FXML
+	private Button btParentesesE;
+	@FXML
+	private Button bt7;
+	@FXML
+	private Button bt5;
+	@FXML
+	private Button bt6;
+	@FXML
+	private Button btVeses;
+	@FXML
+	private Button bt1;
+	@FXML
+	private Button bt2;
+	@FXML
+	private Button bt3;
+	@FXML
+	private Button btMenos;
+	@FXML
+	private Button bt0;
+	@FXML
+	private Button btPonto;
+	@FXML
+	private Button btIgual;
+	@FXML
+	private Button btMais;
+	
+	
+	
+	
+
+}
